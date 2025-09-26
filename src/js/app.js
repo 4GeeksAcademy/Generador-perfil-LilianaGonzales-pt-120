5986cea78db78bf0fac21bbf54b43af634df965b
lili14gon <liligh14@gmail.com>
2025-09-26 21:27:09 +0200

    finalizacion de actividad

--- src/js/app.js
+++ src/js/app.js
@@ -26,21 +26,45 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  // let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  const backgroundDefault =
+    "https://images.unsplash.com/photo-1511974035430-5de47d3b95da";
+  let portada =
+    variables.background == null ? backgroundDefault : variables.background;
+  let cover = `<div class="cover"><img src="${portada}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
+  let name = variables.name == null ? "Lucy" : variables.name;
+  let lastName = variables.lastName == null ? "Boilett" : variables.lastName;
   // reset the website body with the new html output
+  let positionRedes =
+    variables.socialMediaPosition == null
+      ? "position-left"
+      : variables.socialMediaPosition;
+  let rol = variables.role == null ? "Web Developer" : variables.role;
+  let city = variables.city == null ? "Miami" : variables.city;
+  let country = variables.country == null ? "USA" : variables.country;
+  let github = variables.github == null ? "lili14gon" : variables.github;
+  let linkedin =
+    variables.linkedin == null ? "liliana-gonzales" : variables.linkedin;
+  let instagram =
+    variables.instagram == null ? "lili_gon14" : variables.instagram;
+  let twitter = variables.twitter == null ? "4geeksacademy" : variables.twitter;
+  const avatarDefault = "https://randomuser.me/api/portraits/women/42.jpg";
+
+  let avatar =
+    variables.avatarURL == null ? avatarDefault : variables.avatarURL;
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <img src="${avatar}" class="photo" />
+          <h1>${name} ${lastName}</h1>  
+          <h2>${rol}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${positionRedes}">
+            <li><a href="https://twitter.com/${twitter}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +78,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    // background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background: null,
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    // avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: null,
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
